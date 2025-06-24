a2ec6ccaf606891fa8e58150cf38c630dedfcf43
Hasib Ashari <hasibashari@gmail.com>
2025-06-24 22:31:36 +0700

    feat(auth): implement google oauth2 login with passportjs

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
+const passport = require("passport");
+require("./config/passport-setup");
 
 // Initialize the Express application
 const app = express();
@@ -10,6 +12,7 @@ const app = express();
 // Middleware to parse JSON bodies
 app.use(cors())
 app.use(express.json());
+app.use(passport.initialize());
 
 // Specify the port
 const PORT = process.env.PORT || 3001;
